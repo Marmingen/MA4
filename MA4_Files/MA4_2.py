21be8e9d9037721be3b56dd95e73c0233a80bdff
Melker Marminge <mema1030@atterbom.it.uu.se>
2022-10-02 00:48:15 +0200

    test with n=30 and n= 35, nr 2

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -26,25 +26,23 @@ def fib_numba(n):
 def fib_threading_py(lst, func, n):
 	tick = pc()
 	func(n)
-	lst.append((n, pc()-tick))
-	print(lst)
-
+	lst.append(pc()-tick)
 	return lst
 
 def fib_threading_cpp(lst, p, n):
 	tick = pc()
 	p.fib()
-	lst.append((n, pc()-tick))
+	lst.append(pc()-tick)
 	return lst
 
-def plotting(fp, fn, fc):
+def plotting(fp, fn, fc, n_lst):
 	for f in [fp ,fn, fc]:
-		for coord in f:
-			plt.plot(coord[0],coord[1])
+		plt.plot(n_lst, f)
+
 	plt.xlabel("n-values")
 	plt.ylabel("time")
 	plt.title("")
-	#plt.legend()
+	plt.legend()
 
 	plt.savefig("plot.png")	
 
@@ -55,7 +53,9 @@ def main():
 	fn = []
 	fc = []
 
-	for n in range(30, 39, 5):
+	n_lst = range(30,36)
+
+	for n in n_lst:
 		print(f"n-value: {n}")
 		tick = pc()
 
@@ -75,7 +75,7 @@ def main():
 		print(f"total time for {n}: {pc()-tick}")
 		print(fp)
 
-	plotting(fp, fn, fc)
+	plotting(fp, fn, fc, n_lst)
 
 if __name__ == '__main__':
 	main()
